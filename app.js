115184688bc99f48f38f03eec622d82ff81667fb
Amith Pramod <amith.pramod@mindtree.com>
2019-03-18 11:33:02 +0530

    removed localhost to work in heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 const publicDirPath = path.join(__dirname, './public');
@@ -103,6 +105,6 @@ app.get('*',(req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('server running on 3000');
 })
--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,11 @@
 //     })
 // })
 
-fetch('http://localhost:3000/weather?address=mandya').then( (response) => {
-    response.json().then( (data) => {
-        console.log(data);
-    })
-})
+// fetch('http://localhost:3000/weather?address=mandya').then( (response) => {
+//     response.json().then( (data) => {
+//         console.log(data);
+//     })
+// })
 
 
 const formSubmit = document.querySelector('form');
@@ -17,9 +17,11 @@ const locationVal = document.querySelector('input');
 
 formSubmit.addEventListener('submit', (e) =>{
     e.preventDefault();
-    fetch('http://localhost:3000/weather?address='+locationVal.value).then( (response) => {
+    fetch('/weather?address='+locationVal.value).then( (response) => {
     response.json().then( (data) => {
-        console.log(data);
+        if(data.error){
+            document.querySelector('.weather-report').innerHTML = data.error;
+        }
         document.querySelector('.weather-report').innerHTML = data[0].forecast;
     })
 })
